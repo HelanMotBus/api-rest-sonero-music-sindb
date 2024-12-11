ca52fb88576da9706bddfcb0392353eb49659197
HelanMotBus <137359693+HelanMotBus@users.noreply.github.com>
2024-12-10 23:19:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+
 app.use(bodyParser.json());
 
 const leerDatos = () => {
